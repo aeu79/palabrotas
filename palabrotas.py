9325e9a8cdfb97d065d0de726b2cde5f2af7e832
Agustín E. Ure <25160294+aeu79@users.noreply.github.com>
2021-03-25 20:29:07 +0100

    Create English version

--- palabrotas.py
+++ palabrotas.py
@@ -1,3 +1,4 @@
+import argparse
 import itertools
 import time
 import zipfile
@@ -5,12 +6,14 @@ import io
 import os
 
 
-def carga_diccionario():
+def carga_diccionario(language):
     # Podría poner un input. Por ahora hardcoded
     global set_dict
     directorio = os.path.dirname(__file__)
-    zip_path = os.path.join(directorio, "files", "palabras.zip")
-    # zip_path = '/home/agu/Dropbox/Python scripts/palabrotas/palabras.zip'
+    if language == "spanish":
+        zip_path = os.path.join(directorio, "files", "palabras_es.zip")
+    elif language == "english":
+        zip_path = os.path.join(directorio, "files", "palabras_en.zip")
     name = "palabras.txt"
     with zipfile.ZipFile(zip_path, "r") as myzip:
         # zinfo = myzip.namelist() # Esto si no conociera el nombre del archivo
@@ -20,7 +23,8 @@ def carga_diccionario():
             # set_dict = set(['perro','gato','pelo','delfin'])
             set_dict = set(
                 word.split("/")[0].strip() for word in lineas
-            )  # Armo un set (mucho más rápido porque usa hashes). Me quedo solo hasta el '/' y saco el espacio de las que no lo tienen.
+            )  # Armo un set (mucho más rápido porque usa hashes).
+            # Me quedo solo hasta el '/' y saco el espacio de las que no lo tienen.
 
 
 def inicio(modo, letras=None):
@@ -33,21 +37,46 @@ def inicio(modo, letras=None):
     if (
         letras
     ):  # I might have letters already if it is a second round (for example to check shorter words)
-        print("Las letras actuales son: ", ", ".join(letras))
+        if args.spanish:
+            print("Las letras actuales son: ", ", ".join(letras))
+            print("Deje vacío debajo para volver a usarlas.")
+        else:
+            print("Current letters: ", ", ".join(letras))
+            print("Leave empty below to use them again.\n")
         previous = letras
     if modo == "diccionario":
-        letras = input(
-            "Ingrese las letras que deben formar las palabras.\nLetras: "
-        ).lower()
+        if args.spanish:
+            letras = input(
+                "Ingrese las letras que deben formar las palabras.\nLetras ("
+                "q"
+                " para terminar): "
+            ).lower()
+        else:
+            letras = input(
+                "Enter the letters that must be used to generate the words.\nLetters ("
+                "q"
+                " to quit): "
+            ).lower()
         if previous and (letras == ""):  # If empty, use the previous ones.
             letras = previous
+    # Complete mode
     else:
-        letras = input(
-            'Ingrese las letras combinables y palabras para usar enteras después de una "/" (nada para terminar).\nLetras: '
-        ).lower()
-    if letras == "":
+        if args.spanish:
+            letras = input(
+                'Ingrese las letras combinables. Se pueden usar sub-palabras enteras después de un "/" (deje vacío '
+                "para terminar).\nLetras: "
+            ).lower()
+        else:
+            letras = input(
+                "Enter the letters to be combined. Sub-words can be used to be combined with the other letters and "
+                'can must be indicated after a "/" (leave empty to finish).\nLetters: '
+            ).lower()
+    if letras == "" or letras == "q":
         quit()
-    long = input("Longitud de la palabra: ")
+    if args.spanish:
+        long = input("Longitud de la palabra: ")
+    else:
+        long = input("Length of the word: ")
     modo = modo
     palabrotas(modo, letras, long)
 
@@ -57,14 +86,17 @@ def filtrar(candidatas, letras):
     Se encarga de filtrar y recuperar solo las palabras que contienen ciertas
     letras o palabra (Solo en el orden en que se pusieron).
     """
-    filtrada = [
-        palabra for palabra in candidatas if letras in palabra
-    ]  # Toma sólo las que contienen las letras elegidas para filtrar. List comprehension with conditional if... Tomá mate!
+    filtrada = [palabra for palabra in candidatas if letras in palabra]
     print(len(filtrada))
     print(filtrada)  # set para no repetir elementos
     print("\n")
     # Vuelve a preguntar y lanza la función de nuevo (si ingresa nuevas letras):
-    rta = input("Filtrar ahora con estas letras (vacío para terminar):\n")
+    if args.spanish:
+        rta = input("Filtrar ahora con estas letras (vacío para terminar):\n")
+    else:
+        rta = input(
+            "Filter again but with these letters instead (leave empty to finish):\n"
+        )
     if rta != "":
         filtrar(candidatas, rta)
     return
@@ -75,8 +107,6 @@ def palabrotas(modo, letras, longitud):
     old_letras = letras
     # Primero me fijo y separo las palabras que se hayan ingresado con "/"
     letras = letras.split("/")  # If len >1, entonces sí había algo separado por /
-    # letras[0] contiene las letras, 1,2, 3, etc. tienen las palabras para usar en la formación de palabras.
-    print(modo)
     longitud = int(longitud)
     if modo == "completo":
         t = time.time()  # Para tomarle el tiempo
@@ -116,7 +146,8 @@ def palabrotas(modo, letras, longitud):
                     )
                     == longitud
                 ]
-                # combinadas = [''.join(it).replace('1', letras[1]).replace('2', letras[2]).replace('3', letras[3]) for it in itertools.permutations(letras[0].join('123'), longitud)]
+                # combinadas = [''.join(it).replace('1', letras[1]).replace('2', letras[2]).replace('3', letras[3])
+                # for it in itertools.permutations(letras[0].join('123'), longitud)]
             elif len(letras) == 3:  # ['asdfasdf','perro','gato']
                 new_long = str(
                     longitud - len(letras[1]) - len(letras[2]) + 2
@@ -157,25 +188,25 @@ def palabrotas(modo, letras, longitud):
                     "".join(it) for it in itertools.permutations(letras[0], longitud)
                 ]
             else:
-                print("No debería llegar nunca a este punto")
-        """
-        Ahora voy a podría hacer un reemplazo para poder usar una palabra en el medio
-        y no todas las posibles combinaciones [''.join(it).replace('P','J') for it in itertools.permutations('atdP', 3)]
-     
-        combinadas = [''.join(it) for it in itertools.permutations(letras, longitud)] #Las da separadas así que con join uno cada letra en palabras individuales
-           """
+                print(
+                    "No debería llegar nunca a este punto"
+                )  # It shouldn't be possible to get here.
+
         combinadas = set(combinadas)
         candidatas = set(
             word for word in combinadas if word in set_dict
         )  # transformo en set para evitar repetidos: 'zafar', 'zafar' (dos "a" en el input y las considera distintas)
-        print(
-            "Son "
-            + str(len(combinadas))
-            + " posibilidades."
-            + "Tardó "
-            + str(round(time.time() - t, 3))
-            + " segundos en combinarlas."
-        )
+        that_many = str(len(combinadas))
+        took = str(round(time.time() - t, 3))
+        if args.spanish:
+            print(
+                f"Son {that_many} posibilidades. Tardó {took} segundos en combinarlas."
+            )
+        else:
+            print(
+                f"There are {that_many} possibilities. It took {took} seconds to combine them."
+            )
+
     elif modo == "diccionario":
         # Primero filtro por tamaño:
         combinadas = [
@@ -192,25 +223,20 @@ def palabrotas(modo, letras, longitud):
         ]
         # Y verifico que no excedan la cantidad de ocurrencias de cada letra en la palabra.
         frecuencias = [combinables.count(letra) for letra in letters]
-        print(combinables)
-        print(letters)
-        print(frecuencias)
         combinadas = []
         letters = tuple(letters)
-        print(len(candidatas))
-        # candidatas = [palabra for palabra in candidatas if (palabra.count(letra) > frecuencias[letters.index(letra)] for letra in letras)]
         for palabra in candidatas:
             letras = set(palabra)
             resultado = "OK"
             for letra in letras:
                 if (
                     palabra.count(letra) > frecuencias[letters.index(letra)]
-                ):  # letters tiene el tuple de letras combinables únicas. Con index traigo su posicón para buscar en frecuencias el número de ocurrencias.
+                ):  # letters tiene el tuple de letras combinables únicas. Con index traigo su posicón para buscar en
+                    # frecuencias el número de ocurrencias.
                     resultado = "tiene más"
                     break
             if resultado == "OK":
                 combinadas.append(palabra)
-        print(len(combinadas))
         candidatas = sorted(combinadas)
         """
         for word in combinadas:
@@ -220,37 +246,80 @@ def palabrotas(modo, letras, longitud):
                 candidatas = candidatas.append(word)
         combinadas = candidatas # Compatibilidad con el modo completo (por lo que sigue)
         """
-    print("Las principales candidatas (" + str(len(candidatas)) + ") son: ")
+    if args.spanish:
+        print(
+            f"Las principales candidatas ({str(len(candidatas))}) con {longitud} letras son: "
+        )
+    else:
+        print(
+            f"The main candidates ({str(len(candidatas))}) with {longitud} letters are: "
+        )
     print(candidatas)
-    rta = input("Filtrar con estas letras (vacío para terminar):\n")
+    if args.spanish:
+        rta = input("Filtrar con estas letras (vacío para terminar):\n")
+    else:
+        rta = input("Filter out words using these letters (leave empty to finish):\n")
     if (
         rta != ""
     ):  # Uso las letras que ingresa para filtrar los resultados un poco más (y ganarle a Matías)
         filtrar(candidatas, rta)
     if modo != "diccionario":
-        if input("Quiere ver el resto? (s/n) ") == "s":
+        if args.spanish:
+            answer = input("Quiere ver el todas las posibles palabras? (s/n) ")
+        else:
+            answer = input("Do you want to see all of them? (y/n)")
+        if (answer == "s") or (answer == "y"):
             print(set(combinadas))  # set para no repetir elementos
             print("\n")
     inicio(modo, old_letras)
 
 
-if (
-    __name__ == "__main__"
-):  # Por si después quier reusar una función de acá (sin que se ejecute todo).
-    carga_diccionario()  # Carga el diccionario al principio para tenerlo en memoria y poder usarlo varias veces sin recargarlo.
-    print("El diccionario tiene " + str(len(set_dict)) + " palabras.\n")
-    rta = input(
-        'Ingrese "c" para modo completo (generar todos los posibles valores) o nada para modo "diccionario" (ingrese "h" para ayuda)\nModo: '
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-e",
+        "--english",
+        action="store_true",
+        help="Use the English dictionary (default)",
     )
+    group.add_argument(
+        "-s", "--spanish", action="store_true", help="Use the Spanish dictionary"
+    )
+    global args
+    args = parser.parse_args()
+    if args.spanish:
+        carga_diccionario(
+            "spanish"
+        )  # Carga el diccionario al principio para tenerlo en memoria y poder usarlo varias veces sin recargarlo.
+        lang = "Spanish"
+        print(f"El diccionario ({lang}) tiene {str(len(set_dict))} palabras.\n")
+    else:
+        carga_diccionario("english")
+        lang = "English"
+        print(f"The dictionary ({lang}) has {str(len(set_dict))} words.\n")
+
+    if args.spanish:
+        rta = input(
+            'Ingrese "c" para modo completo (generar todos los posibles valores) o nada para modo "diccionario" '
+            "\nModo: "
+        )
+    else:
+        rta = input(
+            'Enter "c" for "complete" mode (all possible combinations).\nLeave empty for "dictionary" mode.\nMode: '
+        )
     if rta == "c":
-        print("Eligió modo completo")
-        inicio("completo")  # En modo completa genera todas las opciones
-    elif rta == "h":
-        print("Ayuda:")
-        print("Hable con el programador...")
+        if args.spanish:
+            print("Eligió modo completo.")
+        else:
+            print("Complete mode.")
         inicio("completo")  # En modo completa genera todas las opciones
     else:
-        print("Eligió modo diccionario")
+        if args.spanish:
+            print("Eligió modo diccionario.")
+        else:
+            print("Dictionary mode.")
+
         inicio(
             "diccionario"
         )  # En modo diccionario sólo evalúa las que coinciden con el diccionario.
